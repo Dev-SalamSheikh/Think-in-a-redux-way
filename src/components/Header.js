ca3d2ae7b24102a79466992ddc0f7bfc99dc2291
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-11-06 02:48:20 +0600

    adding todo functionality done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,68 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import tickImage from "../assets/images/double-tick.png";
 import noteImage from "../assets/images/notes.png";
 import plusImage from "../assets/images/plus.png";
+import { added, allcompleted, clearcompleted } from "../redux/todos/actions";
 
 export default function Header() {
-    return (
-        <div>
-            <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
-                <img src={noteImage} className="w-6 h-6" alt="Add todo" />
-                <input
-                    type="text"
-                    placeholder="Type your todo"
-                    className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
-                />
-                <button
-                    type="submit"
-                    className={`appearance-none w-8 h-8 bg-[url('${plusImage}')] bg-no-repeat bg-contain`}
-                ></button>
-            </form>
+  // Disptach Function
+  const disptach = useDispatch();
 
-            <ul className="flex justify-between my-4 text-xs text-gray-500">
-                <li className="flex space-x-1 cursor-pointer">
-                    <img className="w-4 h-4" src={tickImage} alt="Complete" />
-                    <span>Complete All Tasks</span>
-                </li>
-                <li className="cursor-pointer">Clear completed</li>
-            </ul>
-        </div>
-    );
+  // States
+  const [input, setInput] = useState("");
+
+  //   Handle input
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  //   Form Submitting
+  const submitHandler = (e) => {
+    e.preventDefault();
+    disptach(added(input));
+    setInput("");
+  };
+
+  // Complete All function
+  const completeHandler = () => {
+    disptach(allcompleted());
+  };
+
+  // Clear Completed function
+  const clearHandler = () => {
+    disptach(clearcompleted());
+  };
+
+  return (
+    <div>
+      <form
+        className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+        onSubmit={submitHandler}
+      >
+        <img src={noteImage} className="w-6 h-6" alt="Add todo" />
+        <input
+          type="text"
+          placeholder="Type your todo"
+          className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+          value={input}
+          onChange={handleInput}
+        />
+        <button
+          type="submit"
+          className={`appearance-none w-8 h-8 bg-[url('${plusImage}')] bg-no-repeat bg-contain`}
+        ></button>
+      </form>
+
+      <ul className="flex justify-between my-4 text-xs text-gray-500">
+        <li className="flex space-x-1 cursor-pointer" onClick={completeHandler}>
+          <img className="w-4 h-4" src={tickImage} alt="Complete" />
+          <span>Complete All Tasks</span>
+        </li>
+        <li className="cursor-pointer" onClick={clearHandler}>
+          Clear completed
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/redux/todos/reducer.js
+++ src/redux/todos/reducer.js
@@ -20,6 +20,8 @@ const reducer = (state = initalState, action) => {
         ...state,
         {
           id: nextTodoId(state),
+          text: action.payload,
+          completed: false,
         },
       ];
 
