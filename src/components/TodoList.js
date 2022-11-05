895739e852f029a0eddf58ccee0a50a7d88a74f4
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-11-06 01:33:23 +0600

    todolist and todocolors are dynamic

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
 import Todo from "./Todo";
 
 export default function TodoList() {
-    return (
-        <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-            <Todo />
-        </div>
-    );
+  const todos = useSelector((state) => state.todos);
+  return (
+    <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo.id} />
+      ))}
+    </div>
+  );
 }
