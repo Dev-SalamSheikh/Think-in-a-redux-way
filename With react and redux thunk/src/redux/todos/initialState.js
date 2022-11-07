e167af37481eaa2138a17505ac8acb51c290ea1c
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-11-07 17:06:29 +0600

    react redux with thunk files added

---
+++ With react and redux thunk/src/redux/todos/initialState.js
@@ -0,0 +1,15 @@
+const initialState = [
+    {
+        id: 1,
+        text: "Learn React JS",
+        completed: true,
+    },
+    {
+        id: 2,
+        text: "Learn Redux",
+        completed: false,
+        color: "red",
+    },
+];
+
+export default initialState;
